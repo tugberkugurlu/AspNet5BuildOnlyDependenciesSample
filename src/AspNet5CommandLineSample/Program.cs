99b5f89a3e1aa8258e715a83ecdcf0b5f93bf860
tugberkugurlu <tugberkugurlu@gmail.com>
2015-04-28 08:19:52 +0100

    initial load

---
+++ src/AspNet5CommandLineSample/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.Framework.Runtime.Common.CommandLine;
+
+namespace AspNet5CommandLineSample
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+        }
+    }
+}
